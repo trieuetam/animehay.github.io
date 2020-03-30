6f761d7bd0dbe786676e0f14706741910ccb5082
Trieu Phan <62285742+trieuetam@users.noreply.github.com>
2020-03-30 12:14:04 +0700

    Add files via upload

---
+++ animehay.js
@@ -0,0 +1,26 @@
+x= document.querySelector(".click a");
+x.addEventListener("click",function(){
+    document.querySelector("ul.q").classList.toggle("xuongduoi");
+})
+
+y= document.querySelector("i.chon");
+y.addEventListener("click",function(){
+    document.querySelector("ul.q").classList.toggle("xuongduoi");
+})
+
+z= document.querySelector("i.mui");
+z.addEventListener("click",function(){
+    document.querySelector("ul.w").classList.toggle("xuongduoi2");
+})
+a= document.querySelector("i.mui2");
+a.addEventListener("click",function(){
+    document.querySelector("ul.w2").classList.toggle("xuongduoi22");
+})
+b= document.querySelector("i.mui3");
+b.addEventListener("click",function(){
+    document.querySelector("ul.w3").classList.toggle("xuongduoi23");
+})
+b= document.querySelector("i.mui4");
+b.addEventListener("click",function(){
+    document.querySelector("ul.w4").classList.toggle("xuongduoi24");
+})
